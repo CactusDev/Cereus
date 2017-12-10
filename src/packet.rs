f2afd0fdf030e876f4ef55fe99119a0a5514b536
2Cubed <2Cubed@users.noreply.github.com>
2017-12-10 02:50:38 -0500

    Implement packet structure

---
+++ src/packet.rs
@@ -0,0 +1,46 @@
+
+#[derive(Debug)]
+pub enum Component {
+    Text { data: String },
+    Emoji { data: String },
+    Tag { data: String },
+    URL { data: String },
+}
+
+#[derive(Debug)]
+pub enum Event {
+    Start { new: bool },
+    Follow { success: bool },
+    Subscribe { streak: usize },
+    Host { success: bool },
+    Join { success: bool },
+}
+
+#[derive(Debug)]
+pub enum Packet {
+    Message {
+        text: Vec<Component>,
+        action: bool,
+    },
+    Ban { duration: Option<usize> },
+    Event { kind: Event },
+}
+
+#[derive(Eq, PartialEq, Ord, PartialOrd, Debug)]
+pub enum Role {
+    Banned,
+    User,
+    Subscriber,
+    Moderator,
+    Owner,
+}
+
+#[derive(Debug)]
+pub struct Context {
+    packet: Packet,
+    channel: String,
+    user: Option<String>,
+    role: Option<Role>,
+    target: Option<String>,
+    service: String,
+}
