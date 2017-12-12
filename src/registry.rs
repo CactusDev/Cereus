8d597429430fcbf463e3e4856b91149ecf67eba7
Innectic <innectic@gmail.com>
2017-12-11 22:22:48 -0800

    Merge

--- src/registry.rs
+++ src/registry.rs
@@ -14,13 +14,26 @@ impl ClientRegistry {
         }
     }
 
-    pub fn add(&mut self, events: &[String], client: Sender) {
+    pub fn add(&mut self, events: &[String], client: &Sender) {
         for event in events {
-            // TODO: Fix this
-            // self.clients.entry(event.to_string()).or_insert_with(Vec::new).push(client);
+            self.clients.entry(event.to_string()).or_insert_with(Vec::new).push(client.clone());
         }
     }
 
     pub fn remove(&mut self) {
+    }
+
+    pub fn emit(&mut self, event: String, data: String) {
+        match self.clients.get(&event) {
+            Some(clients) => {
+                for client in clients {
+                    println!("Sending");
+                    client.send(data.clone());
+                }
+            },
+            _ => {
+                println!("Nothing");
+            }
+        }
     }
 }
--- src/server.rs
+++ src/server.rs
@@ -14,25 +14,25 @@ struct Server {
     registry: ClientRegistry
 }
 
-pub fn run_server(host: &str, port: i16) {
-    impl PacketHandler for Server {
-        fn handle_message(&mut self, msg: &ws::Message) -> Response {
-            // self.registry.add(&["test".to_string()], client);
-            let parsed: serde_json::Result<packet::Context> =
-                    serde_json::from_str(&msg.to_string());
+impl PacketHandler for Server {
+    fn handle_message(&mut self, msg: &ws::Message) -> Response {
+        self.registry.add(&[String::from("test")], &self.ws);
+        let parsed: serde_json::Result<packet::Context> =
+                serde_json::from_str(&msg.to_string());
 
-            if parsed.is_err() {
-                return Response::Error("Invalid context.".to_string());
-            }
-
-            let deserialized = parsed.unwrap();
-            println!("{:?}", deserialized);
-
-            return Response::Success(serde_json::to_value(deserialized).unwrap());
+        if parsed.is_err() {
+            return Response::Error(String::from("Invalid context."));
         }
-    }
 
-    impl ws::Handler for Server {
+        let deserialized = parsed.unwrap();
+        println!("{:?}", deserialized);
+
+        return Response::Success(serde_json::to_value(deserialized).unwrap());
+    }
+}
+
+pub fn run_server(host: &str, port: i16) {
+    impl ws::Handler for Server {        
         fn on_message(&mut self, msg: ws::Message) -> ws::Result<()> {
             println!("Received: {}", msg);
 
