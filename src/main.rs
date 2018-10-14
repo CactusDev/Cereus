991ef2eea9c489282e83b974865404f2b342d470
Innectic <innectic@gmail.com>
2018-10-14 00:47:21 -0700

    cleanup: remove random old command comments

--- src/main.rs
+++ src/main.rs
@@ -18,37 +18,6 @@ use packet::*;
 fn main() {
     env_logger::init().unwrap();
 
-    // command!("cactus",
-    // 	"default" => handler!(|_context| {
-    // 		Packet::Message { text: vec! [
-    // 			text!("Ohai! I'm CactusBot! "),
-    // 			emoji!("cactus")
-    // 		], action: false }
-    // 	}),
-    // 	"github" => subcommand!(
-    // 		"defaultT" => handler!(|_context| {
-    // 			Packet::Message { text: vec! [
-    // 				text!("We're open source! Check it out at: "),
-    // 				url!("https://github.com/CactusDev")
-    // 			], action: false }
-    // 		}),
-    // 		"test" => subcommand!(  // cactus github test
-    // 			"default" => handler!(|_context| {
-    // 				Packet::Message { text: vec! [
-    // 					text!("HELLO WORLD I EXIST PLEASE HELP ME")
-    // 				], action: false }
-    // 			})
-    // 		)
-    // 	)
-    // )
-
- 	// let test = handler!("default", |_context| {
-  //   		Packet::Message { text: vec! [
-  //   			text!("Ohai! I'm CactusBot! "),
-  //   			emoji!("cactus")
-  //   		], action: false }
-  //   	});
-
     command!("cactus",
     	"default" => handler!(|_context| {
     		Packet::Message { text: vec! [
@@ -72,38 +41,6 @@ fn main() {
     	)
     )
 
- //    let values = command!(
- //    	"default" => |_context| {
-	// 		Packet::Message { text: vec! [
-	// 			text!("Ohai! I'm CactusBot! "),
-	// 			emoji!("cactus")
-	// 		], action: false }
-	// 	},
-	// 	"docs" => |_context| {
-	// 		Packet::Message { text: vec! [
-	// 			text!("Check out my documentation at "),
-	// 			url!("https://cactusbot.rtfd.org"),
-	// 			text!(".")
-	// 		], action: false }
-	// 	},
-	// 	"twitter" => |_context| {
-	// 		Packet::Message { text: vec! [
-	// 			text!("You can follow the team behind CactusBot at: "),
-	// 			url!("https://twitter.com/CactusDevTeam")
-	// 		], action: false }
-	// 	},
-	// 	"github" => |_context| {
-	// 		Packet::Message { text: vec! [
-	// 			text!("We're opensource! Check us out at: "),
-	// 			url!("https://github.com/CactusDev")
-	// 		], action: false }
-	// 	},
-	// 	"help" => |_context| {
-	// 		Packet::Message { text: vec! [
-	// 			text!("Try our docs (!cactus docs). If you're still having issues, tweet us! (!cactus twitter)")
-	// 		], action: false }
-	// 	}
-	// );
     // let mut w = web::WebServer::new("localhost", 1234);
     // w.listen();
 }
