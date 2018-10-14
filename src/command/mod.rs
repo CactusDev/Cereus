012cbb04cf4cb34068759e62943292214275e6ab
Innectic <innectic@gmail.com>
2018-10-14 00:45:45 -0700

    Better command macro system

--- src/command/mod.rs
+++ src/command/mod.rs
@@ -1,15 +1,61 @@
 
 use packet::*;
+use std::collections::HashMap;
+
+pub type BuiltinCommandHandler = Fn(Context) -> Packet;
+
+pub enum HandlerType {
+	/// Handler type only has a default handler
+	Only(Box<BuiltinCommandHandler>),
+	/// Handler type contains named subcommands
+	SubCommands(HashMap<String, Box<BuiltinCommandHandler>>)
+}
 
 #[macro_export]
 macro_rules! command {
-	($($key:expr => $value:expr),+) => {
+	// ($name:expr, $($key:expr => $value:expr),+) => {
+	// 	{
+	// 		let mut handlers: std::collections::HashMap<&str, $crate::command::BuiltinCommandHandler> = std::collections::HashMap::new();
+	// 		$(handlers.insert($key, Box::new($value));)+
+	// 		handlers
+	// 	}
+	// }
+	($name:expr, $($subcommand:expr => $handler:expr),+) => {
 		{
-			let mut test: std::collections::HashMap<&str, Box<Fn(String) -> $crate::packet::Packet>> = std::collections::HashMap::new();
 			$(
-				test.insert($key, Box::new($value));
+				// And we know that our handlers have been parsed, so we can attempt to turn this into a functional
+				// command structure that we can actually use.
+				println!("{} {}", $name, $subcommand);
+
+				match $handler {
+					$crate::command::HandlerType::Only(_) => println!("1"),
+					$crate::command::HandlerType::SubCommands(_) => println!("2")
+				};
 			)+
-			test
+		}
+	}
+}
+
+#[macro_export]
+macro_rules! handler {
+	// If the handler macro sees just a string and a function (BuiltinCommandHandler), then this is an `Only`.
+	($handler:expr) => {
+		{
+			// Don't really need to do any extra processing here.
+			$crate::command::HandlerType::Only(Box::new($handler))
+		}
+	};
+	// However, if it sees a name, and then a map of string -> functino (BuiltinCommandHandler), then we're handling subcommands.
+	($($key:expr => $handler:expr),+) => {
+		{
+			$(println!("{}", $key);)+
+			
+			// Once again, we don't really need to do anything else here other than package it into the correct type.
+			let mut subcommands: std::collections::HashMap<String, Box<$crate::command::BuiltinCommandHandler>> = std::collections::HashMap::new();
+			$(
+				subcommands.insert($key.to_string(), Box::new($handler));
+			)+
+			$crate::command::HandlerType::SubCommands(subcommands)
 		}
 	}
 }
--- src/main.rs
+++ src/main.rs
@@ -18,38 +18,92 @@ use packet::*;
 fn main() {
     env_logger::init().unwrap();
 
-    let values = command!(
-    	"default" => |_context| {
-			Packet::Message { text: vec! [
-				text!("Ohai! I'm CactusBot")
-			], action: false }
-		},
-		"docs" => |_context| {
-			Packet::Message { text: vec! [
-				text!("Check out my documentation at "),
-				url!("https://cactusbot.rtfd.org"),
-				text!(".")
-			], action: false }
-		},
-		"twitter" => |_context| {
-			Packet::Message { text: vec! [
-				text!("You can follow the team behind CactusBot at: "),
-				url!("https://twitter.com/CactusDevTeam")
-			], action: false }
-		},
-		"github" => |_context| {
-			Packet::Message { text: vec! [
-				text!("We're opensource! Check us out at: "),
-				url!("https://github.com/CactusDev")
-			], action: false }
-		},
-		"help" => |_context| {
-			Packet::Message { text: vec! [
-				text!("Try our docs (!cactus docs). If you're still having issues, tweet us! (!cactus twitter)")
-			], action: false }
-		}
-	);
-	println!("{:?}", values.get("default").unwrap()("".to_string()));
+    // command!("cactus",
+    // 	"default" => handler!(|_context| {
+    // 		Packet::Message { text: vec! [
+    // 			text!("Ohai! I'm CactusBot! "),
+    // 			emoji!("cactus")
+    // 		], action: false }
+    // 	}),
+    // 	"github" => subcommand!(
+    // 		"defaultT" => handler!(|_context| {
+    // 			Packet::Message { text: vec! [
+    // 				text!("We're open source! Check it out at: "),
+    // 				url!("https://github.com/CactusDev")
+    // 			], action: false }
+    // 		}),
+    // 		"test" => subcommand!(  // cactus github test
+    // 			"default" => handler!(|_context| {
+    // 				Packet::Message { text: vec! [
+    // 					text!("HELLO WORLD I EXIST PLEASE HELP ME")
+    // 				], action: false }
+    // 			})
+    // 		)
+    // 	)
+    // )
+
+ 	// let test = handler!("default", |_context| {
+  //   		Packet::Message { text: vec! [
+  //   			text!("Ohai! I'm CactusBot! "),
+  //   			emoji!("cactus")
+  //   		], action: false }
+  //   	});
+
+    command!("cactus",
+    	"default" => handler!(|_context| {
+    		Packet::Message { text: vec! [
+    			text!("Ohai! I'm CactusBot! "),
+    			emoji!("cactus")
+    		], action: false }
+    	}),
+    	"github" => handler!(
+    		"default" => |_context| {
+    			Packet::Message { text: vec! [
+    				text!("We're open source! Check it out at: "),
+    				url!("https://github.com/CactusDev")
+    			], action: false }
+    		},
+    		"cereus" => |_context| {
+    			Packet::Message { text: vec! [
+    				text!("Checkout Cereus at: "),
+    				url!("https://github.com/CactusDev/Cereus")
+    			], action: false }
+    		}
+    	)
+    )
+
+ //    let values = command!(
+ //    	"default" => |_context| {
+	// 		Packet::Message { text: vec! [
+	// 			text!("Ohai! I'm CactusBot! "),
+	// 			emoji!("cactus")
+	// 		], action: false }
+	// 	},
+	// 	"docs" => |_context| {
+	// 		Packet::Message { text: vec! [
+	// 			text!("Check out my documentation at "),
+	// 			url!("https://cactusbot.rtfd.org"),
+	// 			text!(".")
+	// 		], action: false }
+	// 	},
+	// 	"twitter" => |_context| {
+	// 		Packet::Message { text: vec! [
+	// 			text!("You can follow the team behind CactusBot at: "),
+	// 			url!("https://twitter.com/CactusDevTeam")
+	// 		], action: false }
+	// 	},
+	// 	"github" => |_context| {
+	// 		Packet::Message { text: vec! [
+	// 			text!("We're opensource! Check us out at: "),
+	// 			url!("https://github.com/CactusDev")
+	// 		], action: false }
+	// 	},
+	// 	"help" => |_context| {
+	// 		Packet::Message { text: vec! [
+	// 			text!("Try our docs (!cactus docs). If you're still having issues, tweet us! (!cactus twitter)")
+	// 		], action: false }
+	// 	}
+	// );
     // let mut w = web::WebServer::new("localhost", 1234);
     // w.listen();
 }
