f1d6b7e675cb9fa042017cdf37f8395acf1c8f76
Innectic <innectic@gmail.com>
2018-10-25 19:05:05 -0700

    Move other code where it belongs

--- src/main.rs
+++ src/main.rs
@@ -25,46 +25,45 @@ fn main() {
     let config = config::CereusConfiguration::new("cereus.json");
     match config {
         Ok(cfg) => {
+            let mut manager = command::manager::CommandManager::new();
+
+            manager.add_command(command!("cactus",
+                "default" => handler!(|_context| {
+                    Context::message(vec! [
+                        text!("Ohai! I'm CactusBot! "),
+                        emoji!("cactus")
+                    ])
+                }),
+                "github" => handler!(
+                    "default" => handler!(|_context| {
+                        Context::message(vec! [
+                            text!("We're open source! Check it out at: "),
+                            url!("https://github.com/CactusDev")
+                        ])
+                    }),
+                    "cereus" => handler!(|_context| {
+                        Context::message(vec! [
+                            text!("Checkout Cereus at: "),
+                            url!("https://github.com/CactusDev/Cereus")
+                        ])
+                    })
+                )
+            ));
+
+            let logging_handler = handler::logging::LoggingHandler::new();
+            let event_handler = handler::event::EventHandler::new();
+            let command_handler = handler::command::CommandHandler::new("!", manager);
+            let spam_handler = handler::spam::SpamHandler::new();
+            let handler_handler = handler::HandlerHandler::new(vec! [
+                Box::new(logging_handler), Box::new(spam_handler), Box::new(event_handler), Box::new(command_handler)
+            ]);
+
+            let w = web::WebServer::new("localhost", 1234, handler_handler);
+            w.listen();
         },
         Err(e) => {
             println!("Could not start Cereus due to a configuration error.");
             println!("{}", e);
         }
     }
-
-    // let mut manager = command::manager::CommandManager::new();
-
-    // manager.add_command(command!("cactus",
-    // 	"default" => handler!(|_context| {
-    // 		Context::message(vec! [
-    // 			text!("Ohai! I'm CactusBot! "),
-    // 			emoji!("cactus")
-    // 		])
-    // 	}),
-    // 	"github" => handler!(
-    // 		"default" => handler!(|_context| {
-    // 			Context::message(vec! [
-    // 				text!("We're open source! Check it out at: "),
-    // 				url!("https://github.com/CactusDev")
-    // 			])
-    // 		}),
-    // 		"cereus" => handler!(|_context| {
-    // 			Context::message(vec! [
-    // 				text!("Checkout Cereus at: "),
-    // 				url!("https://github.com/CactusDev/Cereus")
-    // 			])
-    // 		})
-    // 	)
-    // ));
-
-    // let logging_handler = handler::logging::LoggingHandler::new();
-    // let event_handler = handler::event::EventHandler::new();
-    // let command_handler = handler::command::CommandHandler::new("!", manager);
-    // let spam_handler = handler::spam::SpamHandler::new();
-    // let handler_handler = handler::HandlerHandler::new(vec! [
-    //     Box::new(logging_handler), Box::new(spam_handler), Box::new(event_handler), Box::new(command_handler)
-    // ]);
-
-    // let w = web::WebServer::new("localhost", 1234, handler_handler);
-    // w.listen();
 }
